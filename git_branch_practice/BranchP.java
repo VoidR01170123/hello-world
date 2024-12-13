ce3786a1099bd88276c3cd37a87f822ac20f5ed9
VoidR01170123 <asj01170123@gmail.com>
2024-12-13 23:37:12 +0900

    practicing

---
+++ git_branch_practice/BranchP.java
@@ -0,0 +1,15 @@
+package git_branch_practice;
+
+public class BranchP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d = 1.424234;
+		int a = 10;
+		
+		int result = (int)d + a;
+	
+
+	}
+
+}
